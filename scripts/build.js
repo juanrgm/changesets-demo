0c328fef2ad80c69b2468e076971de9e1ec9ab54
Juanra GM <juanrgm724@gmail.com>
2022-03-21 22:14:48 +0100

    ci

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,10 @@
-const {copyFileSync} = require('fs')
+const {copyFileSync, readFileSync} = require('fs')
 
 const basePath = `${__dirname}/../packages/base/`
 copyFileSync(basePath + "/package.json", basePath + "/lib/package.json");
 console.log('Copied')
+
+console.log('#############base')
+console.log (readFileSync(basePath + "/package.json").toString())
+console.log('#############lib')
+console.log (readFileSync(basePath + "/lib/package.json").toString())
